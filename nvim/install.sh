d04f585fa7a47aa85b7bbcfcbc89e48c603f85da
Nathan Wright <natiouswright@gmail.com>
2023-09-05 20:21:51 -0500

    Added install script and moved README

---
+++ nvim/install.sh
@@ -0,0 +1,10 @@
+mkdir ~/.config
+mkdir ~/.config/nvim
+mkdir ~/.config/nvim/lua
+mkdir ~/.config/nvim/lua/user
+ln -s ~/.dotfile/nvim/init.lua ~/.config/nvim/init.lua
+ln -s ~/.dotfile/nvim/lua/user/options.lua ~/.config/nvim/lua/user/options.lua
+ln -s ~/.dotfile/nvim/lua/user/keymaps.lua ~/.config/nvim/lua/user/keymaps.lua
+ln -s ~/.dotfile/nvim/lua/user/plugins.lua ~/.config/nvim/lua/user/plugins.lua
+ln -s ~/.dotfile/nvim/lua/user/theme.lua ~/.config/nvim/lua/user/theme.lua
+ln -s ~/.dotfile/nvim/lua/user/cmp.lua ~/.config/nvim/lua/user/cmp.lua
