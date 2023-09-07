fb5f3c317cce6eff7b4114f851433684c36246e8
Nathan Wright <natiouswright@gmail.com>
2023-09-06 21:41:19 -0500

    Added treesitter

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,6 +2,10 @@
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
 sudo dpkg -i ripgrep_13.0.0_amd64.deb
 
+# Install GCC (a c compiler) for treesitter
+sudo apt update
+sudo apt install build-essential
+
 # Symlink nvim config files
 mkdir -p ~/.config/nvim/lua/user
 cp -rs ~/.dotfile/nvim/ ~/.config/nvim/
